d105606ef05c6e9f736bed7b007115bce6029782
Eliseo Silva <eliseogithub256@gmail.com>
2026-08-13 14:46:54 -0600

    Uso de validaciones de edad

--- PrimeraAplicacion/PrimeraAplicacion/Form1.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Form1.cs
@@ -11,40 +11,47 @@ namespace PrimeraAplicacion
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            int edad = int.Parse(txtEdad.Text);
-            if (edad < 0)
+            try
             {
-                lblRespuesta.Text = "Edad no válida";
-            }
-            else if (edad <= 2)
-            {
-                lblRespuesta.Text = "Bebé";
+                int edad = int.Parse(txtEdad.Text);
+                if (edad < 0)
+                {
+                    lblRespuesta.Text = "Edad no válida";
+                }
+                else if (edad <= 2)
+                {
+                    lblRespuesta.Text = "Bebé";
 
-            }
-            else if (edad <= 12)
-            {
-                lblRespuesta.Text = "Niño";
-            }
-            else if (edad <= 17)
-            {
-                lblRespuesta.Text = "Adolescente";
-            }
-            else if (edad <= 65)
-            {
-                lblRespuesta.Text = "Adulto";
-            }
-            else if (edad <= 80)
-            {
-                lblRespuesta.Text = "Adulto mayor";
+                }
+                else if (edad <= 12)
+                {
+                    lblRespuesta.Text = "Niño";
+                }
+                else if (edad <= 17)
+                {
+                    lblRespuesta.Text = "Adolescente";
+                }
+                else if (edad <= 65)
+                {
+                    lblRespuesta.Text = "Adulto";
+                }
+                else if (edad <= 80)
+                {
+                    lblRespuesta.Text = "Adulto mayor";
 
+                }
+                else if (edad <= 130)
+                {
+                    lblRespuesta.Text = "Larga Vida";
+                }
+                else
+                {
+                    lblRespuesta.Text = "Edad no válida";
+                }
             }
-            else if (edad <= 130)
+            catch (Exception)
             {
-                lblRespuesta.Text = "Larga Vida";
-            }
-            else
-            {
-                lblRespuesta.Text = "Edad no válida";
+                lblRespuesta.Text = "Ingrese su edad en numeros enteros";
             }
         }
     }
