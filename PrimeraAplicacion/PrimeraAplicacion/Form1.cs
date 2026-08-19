cca99b47353fe9161f3732a1c41147b48c9e4802
Eliseo Silva <eliseogithub256@gmail.com>
2026-08-19 10:55:04 -0600

    Calculo de la desviacion tipica

--- PrimeraAplicacion/PrimeraAplicacion/Form1.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Form1.cs
@@ -12,21 +12,28 @@ namespace PrimeraAplicacion
             InitializeComponent();
         }
 
-        
-
+        double media(double[] serie) {  
+            double suma = 0;
+            for (int i = 0; i < serie.Length; i++) {
+                suma += serie[i];
+                }
+            double media = suma / serie.Length;
+            return media;
+        }
+        double desviacionTipica(double[] serie, double media) { 
+            return Math.Sqrt(serie.Average(n => Math.Pow(n - media, 2)));
+        }
+        //media armonica
         private void btnProcesar_Click(object sender, EventArgs e)
         {
             try
             {
                 String[] serie = txtSerie.Text.Split(',');
             double[] miSerie = serie.Select(n=>double.Parse(n)).ToArray();
-            double suma = 0;
-            for (int i = 0; i < serie.Length; i++) {
-                suma += miSerie[i];
-                }
-            double media = suma / miSerie.Length;
-            ltsValores.Items.Add("La media es: " + media);
+            double m = media(miSerie);
 
+                ltsValores.Items.Add("La media es: " + Math.Round(m, 3).ToString());
+                ltsValores.Items.Add("La desviación típica es: " + Math.Round(desviacionTipica(miSerie, m), 3).ToString());
             } catch { }
         }
     }
--- PrimeraAplicacion/PrimeraAplicacion/obj/Debug/net10.0-windows/PrimeraAplicacion.AssemblyInfo.cs
+++ PrimeraAplicacion/PrimeraAplicacion/obj/Debug/net10.0-windows/PrimeraAplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeraAplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fc36ef7f312cdd5be1460003e00d0d33bc31b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd41b7088092c410fe3773ebbc08d679efc716c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeraAplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeraAplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
